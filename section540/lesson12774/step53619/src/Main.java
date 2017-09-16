fbfafc6366fc1b19f5f84940182e74a685e581a4
geeksploit <geeksploit@shootan.com>
2017-09-17 00:23:09 +0300

    Define and initialize the ClassA logger.

--- section540/lesson12774/step53619/src/Main.java
+++ section540/lesson12774/step53619/src/Main.java
@@ -4,6 +4,7 @@ class Main {
     //Stepik code: start
     public static void configureLogging() {
         // your implementation here
+        Logger loggerClassA = Logger.getLogger("org.stepic.java.logging.ClassA");
     }
 //Stepik code: end
 }
