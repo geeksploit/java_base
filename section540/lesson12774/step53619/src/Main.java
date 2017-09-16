83748bf9aaeef0d0d195daa0a3f281706ebf0150
geeksploit <geeksploit@shootan.com>
2017-09-17 00:27:55 +0300

    Set the ClassA logger to log all messages.

--- section540/lesson12774/step53619/src/Main.java
+++ section540/lesson12774/step53619/src/Main.java
@@ -5,6 +5,7 @@ class Main {
     public static void configureLogging() {
         // your implementation here
         Logger loggerClassA = Logger.getLogger("org.stepic.java.logging.ClassA");
+        loggerClassA.setLevel(Level.ALL);
     }
 //Stepik code: end
 }
