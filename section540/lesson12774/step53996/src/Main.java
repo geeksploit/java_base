7a617c5955fb5f64685489d0f76ab58db9f31498
geeksploit <geeksploit@shootan.com>
2017-09-24 21:40:48 +0300

    Declare a field to store third party mail services

--- section540/lesson12774/step53996/src/Main.java
+++ section540/lesson12774/step53996/src/Main.java
@@ -1,6 +1,8 @@
 // implement UntrustworthyMailWorker, Spy, Inspector, Thief, StolenPackageException, IllegalPackageException as public static classes here
 public static class UntrustworthyMailWorker implements MailService {
 
+    MailService[] mailServices;
+
     @Override
     public Sendable processMail(Sendable mail) {
         return mail;
