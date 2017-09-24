382b8069c8a0a9f46f1974a864d98346de22b4aa
geeksploit <geeksploit@shootan.com>
2017-09-24 21:38:59 +0300

    Implement mail service interface

--- section540/lesson12774/step53996/src/Main.java
+++ section540/lesson12774/step53996/src/Main.java
@@ -1,3 +1,8 @@
 // implement UntrustworthyMailWorker, Spy, Inspector, Thief, StolenPackageException, IllegalPackageException as public static classes here
-public static class UntrustworthyMailWorker {
+public static class UntrustworthyMailWorker implements MailService {
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        return mail;
+    }
 }
