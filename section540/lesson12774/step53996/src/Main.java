86485524b2924189c194aa2b854c38fde17851e2
geeksploit <geeksploit@shootan.com>
2017-09-24 22:19:27 +0300

    Return the newly packed mail for further processing

--- section540/lesson12774/step53996/src/Main.java
+++ section540/lesson12774/step53996/src/Main.java
@@ -72,7 +72,7 @@ public static class Thief implements MailService {
         stolenValue += content.getPrice();
         Package newPackage = new Package("stones instead of " + content.getContent(), 0);
         MailPackage newMailPackage = new MailPackage(mailPackage.getFrom(), mailPackage.getTo(), newPackage);
-        return mail;
+        return newMailPackage;
     }
 
     public int getStolenValue() {
