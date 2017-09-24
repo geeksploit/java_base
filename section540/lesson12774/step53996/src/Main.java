93dce203d02560a72257195c663567e85fc64a25
geeksploit <geeksploit@shootan.com>
2017-09-24 21:41:50 +0300

    Define a constructor accepting an array of third party mail services

--- section540/lesson12774/step53996/src/Main.java
+++ section540/lesson12774/step53996/src/Main.java
@@ -3,6 +3,10 @@ public static class UntrustworthyMailWorker implements MailService {
 
     MailService[] mailServices;
 
+    public UntrustworthyMailWorker(MailService... mailServices) {
+        this.mailServices = mailServices;
+    }
+
     @Override
     public Sendable processMail(Sendable mail) {
         return mail;
