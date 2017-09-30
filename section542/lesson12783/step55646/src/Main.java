382520e4c1dab124161638d94ab0cd07b857be0d
geeksploit <geeksploit@shootan.com>
2017-09-30 17:54:00 +0300

    Define a local variable to store the next byte

--- section542/lesson12783/step55646/src/Main.java
+++ section542/lesson12783/step55646/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String... args) throws IOException{
         int prev = System.in.read();
         while (prev != -1) {
+            int next = System.in.read();
         }
     }
 }
