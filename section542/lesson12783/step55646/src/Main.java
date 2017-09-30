de10d02cd6c7a994ea6686d79377f11f02490902
geeksploit <geeksploit@shootan.com>
2017-09-30 17:53:14 +0300

    Add input stream processing loop stub

--- section542/lesson12783/step55646/src/Main.java
+++ section542/lesson12783/step55646/src/Main.java
@@ -3,5 +3,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String... args) throws IOException{
         int prev = System.in.read();
+        while (prev != -1) {
+        }
     }
 }
