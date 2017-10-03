d496368ef1ab2ce5b81e3a9447641f5836cb052a
geeksploit <geeksploit@shootan.com>
2017-10-03 14:46:50 +0300

    Define an input stream reader

--- section542/lesson12784/step55673/src/Main.java
+++ section542/lesson12784/step55673/src/Main.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 
 class Main {
     //Stepik code: start
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         // your implementation here
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, charset);
         return "";
     }
 //Stepik code: end
